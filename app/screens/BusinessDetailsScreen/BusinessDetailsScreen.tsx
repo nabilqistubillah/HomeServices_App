f459482dad0098edad96df2597ab558523d8518b
nabilqistubillah <bilqisbill02@gmail.com>
2025-06-10 21:51:47 +0700

    +BusinessDetailScreen

--- app/screens/BusinessDetailsScreen/BusinessDetailsScreen.tsx
+++ app/screens/BusinessDetailsScreen/BusinessDetailsScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation, useRoute, RouteProp } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '@/app/Utils/Colors';
 import Entypo from '@expo/vector-icons/Entypo';
-import Heading from '@/app/Components/Heading';
+import BusinessAboutMe from './BusinessAboutMe';
+import BusinessPhotos from './BusinessPhotos';
 
 type BusinessDetailsScreenRouteParams = {
   business: any; // Replace 'any' with your actual business type if available
@@ -19,7 +20,8 @@ export default function BusinessDetailsScreen() {
     
   }, [])
   return business&&(
-    <View>
+  <View>
+    <ScrollView style={{height:'93%'}}>
       <TouchableOpacity style={styles.backBtnContainer}
         onPress={()=>navigation.goBack()}>
         <Ionicons name="arrow-back-outline" size={30} color="white"/>
@@ -35,23 +37,28 @@ export default function BusinessDetailsScreen() {
         <Text style={{color:Colors.PRIMARY, backgroundColor:Colors.PRIMARY_LIGHT, padding:5, borderRadius:5, fontSize:14}}>{business?.category.name}</Text>
         </View>
         <Text style={{fontSize:17, fontFamily:'Outfit-Regular', color:Colors.GRAY}}>
-          <Entypo name="location" size={25} color={Colors.PRIMARY} />{business?.address}</Text>
+          <Entypo name="location" size={20} color={Colors.PRIMARY} />{business?.address}</Text>
 
           {/*HorizontalLine*/}
           <View style={{borderWidth:0.4, borderColor:Colors.GRAY, marginTop:20, marginBottom:20}}></View>
           {/*bagian: about me */}
-          <View>
-            <Heading text={'About Me'}/>
-            <Text style={{fontFamily:'Outfit-Regular', lineHeight:28,color:Colors.GRAY, fontSize:16}} numberOfLines={isReadMore?20:5}>{business.about}</Text>
-            <TouchableOpacity onPress={()=>setIsReadMore(!isReadMore)}>
-            <Text style={{color:Colors.PRIMARY, fontSize:16, fontFamily:'Outfit-Regular'}}>{isReadMore?'Read Less':'Read More'}</Text>
-            </TouchableOpacity>
-          </View>
+          <BusinessAboutMe business={business}/>
 
           {/*HorizontalLine*/}
           <View style={{borderWidth:0.4, borderColor:Colors.GRAY, marginTop:20, marginBottom:20}}></View>
+
+          <BusinessPhotos business={business}/>
       </View>
+    </ScrollView>
+    <View style={{display:'flex', flexDirection:'row'}}>
+      <TouchableOpacity style={styles.massagebtn}>
+        <Text style={{textAlign:'center', fontFamily:'Outfit-Medium', color:Colors.PRIMARY, fontSize:18}}>Massage</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.bookingbtn}>
+        <Text style={{textAlign:'center', fontFamily:'Outfit-Medium', color:Colors.WHITE, fontSize:18}}>Massage</Text>
+      </TouchableOpacity>
     </View>
+  </View>
   )
 }
 
@@ -71,6 +78,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap:5,
     alignItems: 'center',
+  },
+  massagebtn:{
+    padding:15,
+    backgroundColor:Colors.WHITE,
+    borderWidth:1,
+    borderColor:Colors.PRIMARY,
+    borderRadius:99,
+  },
+  bookingbtn:{
+    padding:15,
+    backgroundColor:Colors.PRIMARY,
+    borderWidth:1,
+    borderColor:Colors.PRIMARY,
+    borderRadius:99,
   },
 
 })
